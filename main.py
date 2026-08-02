7bbe27fa6737a63c66f431112c2cad34691d259a
wobblygooseberry <brentdwayne10@hotmail.com>
2026-08-02 01:31:48 -0500

    starter asteroid files

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        pygame.display.flip()
+
+        for event in pygame.get.event()
+            if event.type == pygame.QUIT:
+                return
+
+if __name__ == "__main__":
+    main()
